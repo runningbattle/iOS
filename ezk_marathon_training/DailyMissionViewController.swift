1fe413ea6ffd610cad0bd3a10e6bb50cd86ab4d5
Kazuo <kazuo@planningdev.com>
2017-10-02 20:07:29 +0900

    真・PagingMenuController基本的なページの完成

---
+++ ezk_marathon_training/DailyMissionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DailyMissionViewController.swift
+//  ezk_marathon_training
+//
+//  Created by 山本和夫 on 2017/10/02.
+//  Copyright © 2017年 Kazuki Kanamaru. All rights reserved.
+//
+
+import UIKit
+
+class DailyMissionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
